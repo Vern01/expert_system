212683efc84183340bc921529cde4af388add0ad
rlutsch <ross.lutsch@gmail.com>
2016-10-11 10:59:36 +0200

    maths

---
+++ do_math.php
@@ -0,0 +1,37 @@
+<?php
+function do_or($a, $b)
+{
+	if ($a == 1 || $b == 1)
+		return 1;
+	elseif ($a == -1 || $b == -1)
+		return -1;
+	else
+		return 0;
+}
+
+function do_and($a, $b)
+{
+	if ($a == 1 && $b == 1)
+		return 1;
+	else 
+		return 0;
+}	
+
+function do_eor($a, $b)
+{
+	if ($a == 0 || $b == 0)
+		return 0;
+	 elseif ($a == do_not($b))
+		return 1;
+	else 
+		return -1;
+}
+
+function do_not($a)
+{
+	if ($a == 0)
+		return 0;
+	else
+		return -$a;
+}
+?>
