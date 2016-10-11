a257e7629e54796a058c777e1476396942f1467c
Vern01 <verdunw2@gmail.com>
2016-10-11 12:59:33 +0200

    simple solve close to complete

--- simple_solve.php
+++ simple_solve.php
@@ -1,13 +1,40 @@
 <?php
-function simple_solve($data, $pvalue)
+function simple_solve(&$data, &$pvalue)
 {
-    for ($y = 0; $data[$y]; $y++)
+    $size = count($data);
+    $new = $size;
+    while ($size > $new)
     {
-        if (all_accounted())
+        $size = $new;
+        for ($i=0; $i < $size; $i++)
+        {
+            if (all_accounted($data[$i], $pvalue))
+            {
+                get_answer($data[$i], $pvalue);//Finish
+                remove_row($data, $i);
+            }
+        }
+        $new = count($data);
     }
 }
+
+function remove_row(&$data, $index)
+{
+    array_splice($data, $index, 1);
+}
+
+function get_answer($data, &$pvalue)
+{
+        //Ross code..
+}
+
 function all_accounted($array, $defarray)
 {
-    
+    for ($i=0; $i < count($array); $i += 2)
+    {
+        if ($pvalue[$array[$i]] == 0)
+            return (false);
+    }
+    return (true);
 }
 ?>
